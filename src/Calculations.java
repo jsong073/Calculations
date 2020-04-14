e8c0a23e996ec43442109d0e56c121032beef7f2
jsong073 <jaehasong073@gmail.com>
2020-04-14 14:37:32 -0400

    finished the rest of the assignment

--- src/Calculations.java
+++ src/Calculations.java
@@ -6,6 +6,9 @@ public class Calculations {
         int num2 = 0;
         int sum;
         int product;
+        int num3 = 0;
+        String phrase1;
+        String phrase2 ="";
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a number: " );
@@ -18,6 +21,18 @@ public class Calculations {
         product = num1 * num2;
         System.out.println("Sum: " + sum);
         System.out.println("Product: " + product);
+
+        System.out.print("Enter a number: ");
+        num3 = input.nextInt();
+        System.out.println("The square of that number is: " + (num3 * num3));
+
+        System.out.print("Enter a phrase: ");
+        input.nextLine();           //If this line isn't present, then I can't assign a string to phrase1
+        phrase1 = input.nextLine();
+        System.out.print("Enter a phrase: ");
+        phrase2 = input.nextLine();
+
+        System.out.println("Your combined phrase is: " + phrase1 + " " + phrase2);
 
     }
 }
